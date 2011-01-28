f4d108e0d2f7641e8310f46aca49cc05b36232bf
Julivan <julivandamaceno@gmail.com>
2011-01-28 18:56:08 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leilao::Application.config.secret_token = '73aa3abcf635c88230dc18cc7b27545c43230356443086d36b362462e0c9693f6beb740f03ff0a30e0d2b19ee19067784661ad17855d950eea8ad06b40796d55'
